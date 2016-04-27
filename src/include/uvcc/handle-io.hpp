7b559850e60c2aceaa2425f3c05ec19bbd6d6e79
Mike Usenko <mmaxs@hotmail.ru>
2016-04-27 23:57:07 +0300

    WIP on `io` class

--- src/include/uvcc/handle-io.hpp
+++ src/include/uvcc/handle-io.hpp
@@ -11,6 +11,7 @@
 #include <cstddef>      // size_t
 #include <functional>   // function
 #include <string>       // string
+#include <mutex>        // lock_guard
 
 
 namespace uv
@@ -29,17 +30,21 @@ class io : public handle
 public: /*types*/
   using on_read_t = std::function< void(io _handle, ssize_t _nread, buffer _buffer) >;
   /*!< \brief The function type of the callback called by `read_start()` when data was read from an I/O endpoint.
-       \note The libuv API calls user provided read callback functions with a _null-initialized_ `uv_buf_t`
-       buffer structure (where `buf->base = nullptr` and `buf->len = 0`) on error and EOF and does not try to retrieve
-       something from the [`uv_alloc_cb`](http://docs.libuv.org/en/v1.x/handle.html#c.uv_alloc_cb) callback in
-       such a cases, so the uvcc `on_read_t` callback is supplied with a dummy _null-initialized_ `_buffer`. */
+       \note On error and EOF state the libuv API calls the user provided
+       [`uv_read_cb`](http://docs.libuv.org/en/v1.x/stream.html#c.uv_read_cb) function with
+       a _null-initialized_ [`uv_buf_t`](http://docs.libuv.org/en/v1.x/misc.html#c.uv_buf_t) buffer structure
+       (where `buf->base = nullptr` and `buf->len = 0`) and does not try to retrieve  something from the
+       [`uv_alloc_cb`](http://docs.libuv.org/en/v1.x/handle.html#c.uv_alloc_cb) callback in such a cases.
+       So the uvcc `io::on_read_t` callback is supplied with a dummy _null-initialized_ `_buffer`.
+       \sa libuv API documentation: [`uv_read_cb`](http://docs.libuv.org/en/v1.x/stream.html#c.uv_read_cb) */
 
 protected: /*types*/
   //! \cond
   struct property : virtual handle::property
   {
-    bool read_flag = false;
-    on_buffer_t alloc_cb;
+    spinlock rdstate_switch;
+    bool rdstate_flag = false;
+    on_alloc_t alloc_cb;
     on_read_t read_cb;
     virtual int read_start() const noexcept = 0;
     virtual int read_stop() const noexcept = 0;
@@ -77,42 +82,83 @@ private: /*functions*/
 #endif
 
 public: /*interface*/
+  on_alloc_t& on_alloc() const noexcept  { return static_cast< instance* >(ptr)->property->alloc_cb; }
+  on_read_t& on_read() const noexcept  { return static_cast< instance* >(ptr)->property->read_cb; }
+
   /*! \brief Start reading incoming data from the I/O endpoint.
       \details The handle is tried to be set for reading if only nonempty `_alloc_cb` and `_read_cb` functions
-      are  provided, or else `UV_EINVAL` is returned with no involving any libuv API or uvcc function.
-      Repeated call to this function results in the automatic call to `read_stop()` firstly,
-      and `_alloc_cb` function can be empty in this case, which means that it doesn't change from the previous call.
-      \note This function adds an extra reference to the handle instance, which is released when the counterpart
-      function `read_stop()` is called. */
-  int read_start(const on_buffer_t &_alloc_cb, const on_read_t &_read_cb) const
+      are provided, or else `UV_EINVAL` is returned with no involving any libuv API or uvcc function.
+      Repeated call to this function results in the automatic call to `read_stop()` firstly.
+      In the repeated calls `_alloc_cb` and/or `_read_cb` functions can be empty values, which means that
+      they aren't changed from the previous call.
+      \note This function adds an extra reference to the handle instance, which is released when the
+      counterpart function `read_stop()` is called. */
+  int read_start(const on_alloc_t &_alloc_cb, const on_read_t &_read_cb) const
   {
-    if (!_read_cb)  return uv_status(UV_EINVAL);
-
     auto p = static_cast< instance* >(ptr)->property;
 
-    if (!_alloc_cb and !p->on_read)  return uv_status(UV_EINVAL);
+    std::lock_guard< decltype(p->rdstate_switch) > lk(p->rdstate_switch);
+
+    if (!_alloc_cb and !p->alloc_cb)  return uv_status(UV_EINVAL);
+    if (!_read_cb and !p->read_cb)  return uv_status(UV_EINVAL);
 
     static_cast< instance* >(ptr)->ref();  // first, make sure it would exist for the future _read_cb() calls until read_stop()
-    if (p->on_read)  read_stop();
 
-    if (_alloc_cb)  p->on_buffer = _alloc_cb;
-    p->on_read = _read_cb;
+    if (p->rdstate_flag)
+    {
+      uv_status(p->read_stop());
+      static_cast< instance* >(ptr)->unref();  // release the excess reference from the repeated read_start()
+    }
+    else p->rdstate_flag = true;
+
+    if (_alloc_cb)  p->alloc_cb = _alloc_cb;
+    if (_read_cb)  p->read_cb = _read_cb;
 
     uv_status(0);
     int o = p->read_start();
     if (!o)  uv_status(o);
     return o;
   }
+  /*! \brief Restart reading incoming data from the I/O endpoint using `_alloc_cb` and `_read_cb`
+      functions provided with the previous `read_start()` call.
+      Repeated call to this function results in the automatic call to `read_stop()` firstly.
+      \note This function adds an extra reference to the handle instance, which is released when the
+      counterpart function `read_stop()` is called. */
+  int read_start() const
+  {
+    auto p = static_cast< instance* >(ptr)->property;
+
+    std::lock_guard< decltype(p->rdstate_switch) > lk(p->rdstate_switch);
+
+    if (!p->alloc_cb or !p->read_cb)  return uv_status(UV_EINVAL);
+
+    static_cast< instance* >(ptr)->ref();
+
+    if (p->rdstate_flag)
+    {
+      uv_status(p->read_stop());
+      static_cast< instance* >(ptr)->unref();
+    }
+    else p->rdstate_flag = true;
+
+    uv_status(0);
+    int o = p->read_start();
+    if (!o)  uv_status(o);
+    return o;
+  }
+
   /*! \brief Stop reading data from the I/O endpoint. */
   int read_stop() const
   {
     auto p = static_cast< instance* >(ptr)->property;
 
+    std::lock_guard< decltype(p->rdstate_switch) > lk(p->rdstate_switch);
+
     uv_status(p->read_stop());
 
-    if (p->on_read)
+    if (p->rdstate_flag)
     {
-      p->on_read = on_read_t();
+      p->rdstate_flag = false;
       static_cast< instance* >(ptr)->unref();  // release the excess reference from read_start()
     };
 
