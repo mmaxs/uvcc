b18e9ec5eee9aa5832b1310d7ca823b6210f4e96
Mike Usenko <mmaxs@hotmail.ru>
2016-05-11 22:57:29 +0300

    WIP on `fs::write` request class

--- src/include/uvcc/request-fs.hpp
+++ src/include/uvcc/request-fs.hpp
@@ -144,7 +144,7 @@ public: /*interface*/
   /*! \brief The file which this read request is running on.
       \details It is guaranteed that it will be a valid instance at least within the request callback. */
   uv::file file() const noexcept  { return uv::file(instance::from(uv_req)->properties().uv_handle); }
-  /*! \brief The offset this read request has run at. */
+  /*! \brief The offset this read request has been performed at. */
   int64_t offset() const noexcept  { return instance::from(uv_req)->properties().offset; }
 
   /*! \brief Run the request. Read data from the `_file` into the buffers described by `_buf` object.
@@ -208,7 +208,112 @@ void fs::read::read_cb(::uv_fs_t *_uv_req)
 /*! \brief Write data to a file. */
 class fs::write : public fs
 {
+  //! \cond
+  friend class request::instance< write >;
+  //! \endcond
+
+public: /*types*/
+  using on_request_t = std::function< void(write _request, buffer _buffer) >;
+  /*!< \brief The function type of the callback called when data was written to the file. */
+
+protected: /*types*/
+  //! \cond
+  struct properties
+  {
+    uv::file::uv_t *uv_handle = nullptr;
+    buffer::uv_t *uv_buf = nullptr;
+    int64_t offset = -1;
+  };
+  //! \endcond
+
+private: /*types*/
+  using instance = request::instance< write >;
+
+private: /*constructors*/
+  explicit write(uv_t *_uv_req)
+  {
+    if (_uv_req)  instance::from(_uv_req)->ref();
+    uv_req = _uv_req;
+  }
+
+public: /*constructors*/
+  ~write() = default;
+  write()  { uv_req = instance::create(); }
+
+  write(const write&) = default;
+  write& operator =(const write&) = default;
+
+  write(write&&) noexcept = default;
+  write& operator =(write&&) noexcept = default;
+
+private: /*functions*/
+  template< typename = void > static void write_cb(::uv_fs_t*);
+
+public: /*interface*/
+  const on_request_t& on_request() const noexcept  { return instance::from(uv_req)->request_cb_storage.value(); }
+        on_request_t& on_request()       noexcept  { return instance::from(uv_req)->request_cb_storage.value(); }
+
+  /*! \brief The file which this write request is running on.
+      \details It is guaranteed that it will be a valid instance at least within the request callback. */
+  uv::file file() const noexcept  { return uv::file(instance::from(uv_req)->properties().uv_handle); }
+  /*! \brief The offset this write request has been performed at. */
+  int64_t offset() const noexcept  { return instance::from(uv_req)->properties().offset; }
+
+  /*! \brief Run the request. Write data to the `_file` from the buffers described by `_buf` object.
+      \sa libuv API documentation: [`uv_fs_write()`](http://docs.libuv.org/en/v1.x/fs.html#c.uv_fs_write).
+      \sa Linux: [`pwritev()`](http://man7.org/linux/man-pages/man2/pwritev.2.html).
+      \note If the request callback is empty (has not been set), the request runs _synchronously_
+      (and the `_loop` parameter is ignored).
+      In this case the function returns a number of bytes written or relevant
+      [libuv error constant](http://docs.libuv.org/en/v1.x/errors.html#error-constants).*/
+  int run(uv::file _file, buffer &_buf, int64_t _offset = -1)
+  {
+    auto instance_ptr = instance::from(uv_req);
+
+    auto &request_cb = instance_ptr->request_cb_storage.value();
+    if (request_cb)
+    {
+      file::instance::from(_file.uv_handle)->ref();
+      buffer::instance::from(_buf.uv_buf)->ref();
+      instance_ptr->ref();
+
+      instance_ptr->properties() = {static_cast< file::uv_t* >(_file), _buf.uv_buf, _offset};
+    };
+
+    uv_status(0);
+    int ret = ::uv_fs_write(
+        static_cast< uv::file::uv_t* >(_file)->loop, static_cast< uv_t* >(uv_req),
+        _file.fd(),
+        static_cast< const buffer::uv_t* >(_buf), _buf.count(),
+        _offset,
+        request_cb ? static_cast< ::uv_fs_cb >(write_cb) : nullptr
+    );
+    if (!ret)  uv_status(ret);
+    return ret;
+  }
+
+public: /*conversion operators*/
+  explicit operator const uv_t*() const noexcept  { return static_cast< const uv_t* >(uv_req); }
+  explicit operator       uv_t*()       noexcept  { return static_cast<       uv_t* >(uv_req); }
 };
+
+template< typename >
+void fs::write::write_cb(::uv_fs_t *_uv_req)
+{
+  auto instance_ptr = instance::from(_uv_req);
+  instance_ptr->uv_error = _uv_req->result;
+
+  auto &properties = instance_ptr->properties();
+
+  ref_guard< file::instance > unref_file(*file::instance::from(properties.uv_handle), adopt_ref);
+  ref_guard< instance > unref_req(*instance_ptr, adopt_ref);
+
+  auto &write_cb = instance_ptr->request_cb_storage.value();
+  if (write_cb)
+    write_cb(write(_uv_req), buffer(properties.uv_buf, adopt_ref));
+  else
+    buffer::instance::from(properties.uv_buf)->unref();
+}
 
 
 }
