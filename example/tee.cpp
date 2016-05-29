ca8912931c15a15f1fc7460a78aab1af64045172
Mike Usenko <mmaxs@hotmail.ru>
2016-05-30 00:25:27 +0300

    update example

--- example/tee.cpp
+++ example/tee.cpp
@@ -16,6 +16,10 @@
 uv::io in = uv::io::guess_handle(uv::loop::Default(), fileno(stdin)),
        out = uv::io::guess_handle(uv::loop::Default(), fileno(stdout));
 
+constexpr const std::size_t WRITE_QUEUE_SIZE_UPPER_LIMIT = 500*1024*1024,
+                            WRITE_QUEUE_SIZE_LOWER_LIMIT =  10*1024*1024;
+std::size_t all_write_queues_size = 0;
+
 std::vector< uv::file > files;
 
 
@@ -48,7 +52,7 @@ int main(int _argc, char *_argv[])
   }
 
   in.read_start(
-      [](uv::handle, std::size_t) -> uv::buffer
+      /* [](uv::handle, std::size_t) -> uv::buffer
       {
         constexpr const std::size_t default_size = 8192;
         static std::vector< uv::buffer > buf_pool;
@@ -72,7 +76,8 @@ int main(int _argc, char *_argv[])
         fprintf(stderr, "[buffer pool]: new item #%zu\n", buf_pool.size());  fflush(stderr);
         #endif
         return buf_pool.back();
-      },
+      }, */
+      [](uv::handle, std::size_t _suggested_size) -> uv::buffer  { return uv::buffer{_suggested_size}; },
       [](uv::io _io, ssize_t _nread, uv::buffer _buf, void*) -> void
       {
         if (_nread < 0)
@@ -85,26 +90,38 @@ int main(int _argc, char *_argv[])
           _buf.len() = _nread;
 
           uv::output wr;
-          wr.on_request() = [](uv::output _wr, uv::buffer) -> void
+          wr.on_request() = [](uv::output _wr, uv::buffer _buf) -> void
           {
             if (!_wr)
             {
               PRINT_UV_ERR("output", _wr.uv_status());
               in.read_stop();
             };
+
+            //all_write_queues_size -= _buf.len();
+            //in.read_resume(all_write_queues_size <= WRITE_QUEUE_SIZE_LOWER_LIMIT);
           };
           wr.run(out, _buf);
           if (!wr)  PRINT_UV_ERR("output::run", wr.uv_status());
 
-          for (auto &f : files)
+          //all_write_queues_size = out.write_queue_size();
+
+          for (auto &file : files)
           {
             uv::fs::write file_wr;
-            file_wr.on_request() = [](uv::fs::write _file_wr, uv::buffer) -> void
+            file_wr.on_request() = [](uv::fs::write _file_wr, uv::buffer _buf) -> void
             {
               if (!_file_wr)  PRINT_UV_ERR("fs::write", _file_wr.uv_status());
+
+              //all_write_queues_size -= _buf.len();
+              //in.read_resume(all_write_queues_size <= WRITE_QUEUE_SIZE_LOWER_LIMIT);
             };
-            file_wr.run(f, _buf);
+            file_wr.run(file, _buf);
+
+            //all_write_queues_size += file.write_queue_size();
           }
+
+          //in.read_pause(all_write_queues_size >= WRITE_QUEUE_SIZE_UPPER_LIMIT);
         };
       }
   );
