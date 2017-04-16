882d901a6b441bd774ec992e353f899f7e45412e
Mike Usenko <mmaxs@hotmail.ru>
2017-04-16 19:43:19 +0300

    WIP on `async`, `timer` handle classes

--- src/include/uvcc/handle-misc.hpp
+++ src/include/uvcc/handle-misc.hpp
@@ -72,14 +72,15 @@ public: /*constructors*/
   explicit async(uv::loop &_loop)
   {
     uv_handle = instance::create();
-    auto uv_ret = uv_status(
-        ::uv_async_init(static_cast< uv::loop::uv_t* >(_loop), static_cast< uv_t* >(uv_handle), nullptr)
-    );
-    if (uv_ret >= 0)  instance::from(uv_handle)->book_loop();
+
+    auto uv_ret = ::uv_async_init(static_cast< uv::loop::uv_t* >(_loop), static_cast< uv_t* >(uv_handle), nullptr);
+    if (uv_status(uv_ret) < 0)  return;
+
+    instance::from(uv_handle)->book_loop();
   }
 
   /*! \brief Create an `async` handle with specified callback function.
-      \details All arguments are copied (or moved) to the internal callback function object. For passing arguments by reference
+      \note All arguments are copied (or moved) to the internal callback function object. For passing arguments by reference
       (when parameters are used as output ones), wrap them with `std::ref()` or use raw pointers.
       \sa libuv API documentation: [`uv_async_init()`](http://docs.libuv.org/en/v1.x/async.html#c.uv_async_init). */
   template< class _Cb_, typename... _Args_,
@@ -88,13 +89,14 @@ public: /*constructors*/
   async(uv::loop &_loop, _Cb_ &&_cb, _Args_&&... _args)
   {
     uv_handle = instance::create();
+
+    auto uv_ret = ::uv_async_init(static_cast< uv::loop::uv_t* >(_loop), static_cast< uv_t* >(uv_handle), async_cb);
+    if (uv_status(uv_ret) < 0)  return;
+
     instance::from(uv_handle)->properties().cb = std::bind(
         std::forward< _Cb_ >(_cb), std::placeholders::_1, std::forward< _Args_ >(_args)...
     );
-    auto uv_ret = uv_status(
-        ::uv_async_init(static_cast< uv::loop::uv_t* >(_loop), static_cast< uv_t* >(uv_handle), async_cb)
-    );
-    if (uv_ret >= 0)  instance::from(uv_handle)->book_loop();
+    instance::from(uv_handle)->book_loop();
   }
 
 public: /*interface*/
@@ -117,6 +119,101 @@ void async::async_cb(::uv_async_t *_uv_handle)
 {
   auto &cb = instance::from(_uv_handle)->properties().cb;
   if (cb)  cb(async(_uv_handle));
+}
+
+
+
+/*! \ingroup doxy_group__handle
+    \brief Timer handle.
+    \sa libuv API documentation: [`uv_timer_t — Timer handle`](http://docs.libuv.org/en/v1.x/timer.html#uv-timer-t-timer-handle). */
+class timer : public handle
+{
+  friend class handle::uv_interface;
+  friend class handle::instance< timer >;
+
+public: /*types*/
+  using uv_t = ::uv_timer_t;
+  template< typename... _Args_ >
+  using on_timer_t = std::function< void(timer _handle, _Args_&&... _args) >;
+  /*!< \brief The function type of the callback called by the timer scheduled with `timer::start()` function.
+       \sa libuv API documentation: [`uv_timer_cb`](http://docs.libuv.org/en/v1.x/timer.html#c.uv_timer_cb). */
+
+protected: /*types*/
+  //! \cond internals
+  //! \addtogroup doxy_group__internals
+  //! \{
+
+  struct properties : handle::properties
+  {
+    std::function< void(timer) > cb;
+  };
+
+  struct uv_interface : handle::uv_handle_interface  {};
+
+  //! \}
+  //! \endcond
+
+private: /*types*/
+  using instance = handle::instance< timer >;
+
+private: /*functions*/
+  template < typename = void > static void timer_cb(::uv_timer_t*);
+
+protected: /*constructors*/
+  //! \cond
+  explicit timer(uv_t *_uv_handle) : handle(reinterpret_cast< handle::uv_t* >(_uv_handle))  {}
+  //! \endcond
+
+public: /*constructors*/
+  ~timer() = default;
+
+  timer(const timer&) = default;
+  timer& operator =(const timer&) = default;
+
+  timer(timer&&) noexcept = default;
+  timer& operator =(timer&&) noexcept = default;
+
+  /*! \brief Create a `timer` handle. */
+  timer(uv::loop &_loop)
+  {
+    uv_handle = instance::create();
+
+    auto uv_ret = ::uv_timer_init(static_cast< uv::loop::uv_t* >(_loop), static_cast< uv_t* >(uv_handle));
+    if (uv_status(uv_ret) < 0)  return;
+
+    instance::from(uv_handle)->book_loop();
+  }
+
+public: /*interface*/
+  /*! \brief Start the timer and schedule the specified callback function.
+      \details `_timeout` and `_repeat` parameters are in milliseconds.
+      \note All arguments are copied (or moved) to the internal callback function object. For passing arguments by reference
+      (when parameters are used as output ones), wrap them with `std::ref()` or use raw pointers.
+      \sa libuv API documentation: [`uv_timer_start()`](http://docs.libuv.org/en/v1.x/timer.html#c.uv_timer_start). */
+  template< class _Cb_, typename... _Args_,
+      typename = std::enable_if_t< std::is_convertible< _Cb_, on_timer_t< _Args_&&... > >::value >
+  >
+  int start(uint64_t _timeout, uint64_t _repeat, _Cb_ &&_cb, _Args_&&... _args) const
+  {
+    instance::from(uv_handle)->properties().cb = std::bind(
+        std::forward< _Cb_ >(_cb), std::placeholders::_1, std::forward< _Args_ >(_args)...
+    );
+
+    return uv_status(
+        ::uv_timer_start(static_cast< uv_t* >(uv_handle), timer_cb, _timeout, _repeat)
+    );
+  }
+
+public: /*conversion operators*/
+  explicit operator const uv_t*() const noexcept  { return static_cast< const uv_t* >(uv_handle); }
+  explicit operator       uv_t*()       noexcept  { return static_cast<       uv_t* >(uv_handle); }
+};
+
+template< typename >
+void timer::timer_cb(::uv_timer_t *_uv_handle)
+{
+  auto &cb = instance::from(_uv_handle)->properties().cb;
+  if (cb)  cb(timer(_uv_handle));
 }
 
 
