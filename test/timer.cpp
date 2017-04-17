0f8934970066e9f5744abb826e88676b62dfeec6
Mike Usenko <mmaxs@hotmail.ru>
2017-04-18 00:27:21 +0300

    update test

--- test/timer.cpp
+++ test/timer.cpp
@@ -16,16 +16,17 @@ int main(int _argc, char *_argv[])
 
   t.start(0, 1000,
       [](uv::timer _t, int &_count){
-        fprintf(stdout, "timer: count=%i repeat=%" PRIi64 "\n", _count, ::uv_timer_get_repeat(static_cast< ::uv_timer_t* >(_t)));
+        fprintf(stdout, "timer: count=%i repeat=%" PRIi64 "\n", _count, _t.repeat_value());
         fflush(stdout);
-        if (--_count <= 0)  ::uv_timer_stop(static_cast< ::uv_timer_t* >(_t));
+        _t.repeat_value(_t.repeat_value()/3);
+        if (--_count <= 0)  _t.stop();
       },
-      count
+      std::ref(count)
   );
 
   uv::loop::Default().run(UV_RUN_DEFAULT);
 
-  fprintf(stdout, "count=%i\n", count);
+  fprintf(stdout, "remaining count: %i\n", count);
   fflush(stdout);
 
   return 0;
